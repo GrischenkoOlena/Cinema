feb28a9654d9a3be5e060dc30790f8f1f6a9c157
GrischenkoOlena <grischenko.olena.v@gmail.com>
2023-01-24 14:39:59 +0200

    BusinessTicketService refactoring

--- src/main/java/com/hryshchenko/cinema/service/business/BusinessTicketService.java
+++ src/main/java/com/hryshchenko/cinema/service/business/BusinessTicketService.java
@@ -6,8 +6,11 @@ import com.hryshchenko.cinema.dto.TicketDTO;
 import com.hryshchenko.cinema.exception.DAOException;
 import com.hryshchenko.cinema.exception.NotEnoughMoneyException;
 import com.hryshchenko.cinema.exception.SeatHasSoldException;
+import com.hryshchenko.cinema.model.dao.TicketDAO;
+import com.hryshchenko.cinema.model.dao.TicketSeatDAO;
+import com.hryshchenko.cinema.model.dao.UserDAO;
+import com.hryshchenko.cinema.model.dbservices.EntityTransaction;
 import com.hryshchenko.cinema.model.dbservices.SeatService;
-import com.hryshchenko.cinema.model.dbservices.TicketService;
 import com.hryshchenko.cinema.model.entity.Seat;
 import com.hryshchenko.cinema.model.entity.Ticket;
 import com.hryshchenko.cinema.model.entity.TicketSeat;
@@ -15,6 +18,8 @@ import com.hryshchenko.cinema.model.entity.User;
 import com.hryshchenko.cinema.service.mapper.MapperSeat;
 import com.hryshchenko.cinema.service.mapper.MapperTicket;
 import com.hryshchenko.cinema.service.mapper.MapperUser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,13 +27,10 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BusinessTicketService {
-
+    private static final Logger log = LogManager.getLogger();
     private static final Lock MUTEX = new ReentrantLock();
     public boolean buyTicket(TicketDTO ticketDTO) throws NotEnoughMoneyException, SeatHasSoldException {
-        double cost = 0;
-        for(SeatDTO temp : ticketDTO.getSeats()){
-            cost += temp.getCategory().getPrice();
-        }
+        double cost = ticketDTO.getSeats().stream().mapToDouble(temp -> temp.getCategory().getPrice()).sum();
 
         Ticket ticket = new MapperTicket().getTicket(ticketDTO);
         User user = new MapperUser().getUser(ticketDTO.getUser());
@@ -45,7 +47,6 @@ public class BusinessTicketService {
         }
         List<Seat> seats = new MapperSeat().getListSeat(ticketDTO.getSeats());
 
-        TicketService ticketService = AppContext.getInstance().getTicketService();
         SeatService seatService = AppContext.getInstance().getSeatService();
         boolean result;
         try {
@@ -56,13 +57,45 @@ public class BusinessTicketService {
                     throw new SeatHasSoldException("unfortunately place has sold");
                 }
             }
-            result = ticketService.createTicket(ticket, ticketHasSeats, user);
+            result = createTicket(ticket, ticketHasSeats, user);
         } catch (DAOException e) {
-            e.printStackTrace();
+            log.error(e.getMessage());
             return false;
         } finally {
             MUTEX.unlock();
         }
         return result;
+    }
+
+    private boolean createTicket(Ticket ticket, List<TicketSeat> seats, User user) throws DAOException{
+        TicketDAO ticketDAO = new TicketDAO();
+        UserDAO userDAO = new UserDAO();
+        TicketSeatDAO ticketSeatDAO = new TicketSeatDAO();
+
+        EntityTransaction entityTransaction = new EntityTransaction();
+
+        entityTransaction.initTransaction(ticketDAO, userDAO, ticketSeatDAO);
+        if (!ticketDAO.create(ticket)){
+            entityTransaction.rollback();
+            entityTransaction.endTransaction();
+            return false;
+        }
+        long ticket_id = ticketDAO.findNextAutoIncrement();
+        for(TicketSeat seat : seats){
+            seat.setTicketId(ticket_id);
+            if(!ticketSeatDAO.create(seat)){
+                entityTransaction.rollback();
+                entityTransaction.endTransaction();
+                return false;
+            }
+        }
+        if(!userDAO.update(user)){
+            entityTransaction.rollback();
+            entityTransaction.endTransaction();
+            return false;
+        }
+        entityTransaction.commit();
+        entityTransaction.endTransaction();
+        return true;
     }
 }
