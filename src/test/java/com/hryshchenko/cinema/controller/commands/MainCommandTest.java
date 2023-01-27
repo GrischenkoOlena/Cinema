9416e620c656f5d92959d8641c7b27ef4202d54b
GrischenkoOlena <grischenko.olena.v@gmail.com>
2023-01-27 23:54:56 +0200

    MainCommandTest added

---
+++ src/test/java/com/hryshchenko/cinema/controller/commands/MainCommandTest.java
@@ -0,0 +1,51 @@
+package com.hryshchenko.cinema.controller.commands;
+
+import com.hryshchenko.cinema.constant.Path;
+import com.hryshchenko.cinema.exception.DAOException;
+import com.hryshchenko.cinema.exception.MapperException;
+import com.hryshchenko.cinema.model.dbservices.ScreeningService;
+import com.hryshchenko.cinema.service.mapper.MapperScreening;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MainCommandTest {
+    @Mock
+    HttpServletRequest req;
+
+    @Mock
+    HttpServletResponse resp;
+
+    @Mock
+    ScreeningService service;
+
+    @Mock
+    MapperScreening mapper;
+
+    @InjectMocks
+    MainCommand command;
+
+    @BeforeEach
+    void setUp() throws DAOException, MapperException{
+        Mockito.when(req.getParameter("scheduleDate")).thenReturn("25/01/2023");
+        Mockito.when(service.getScreeningByDate(Mockito.any())).thenReturn(new ArrayList<>());
+        Mockito.when(mapper.getListDTO(Mockito.any())).thenReturn(new ArrayList<>());
+    }
+
+    @Test
+    void execute()  {
+        assertEquals(Path.MAIN, command.execute(req, resp));
+    }
+}
