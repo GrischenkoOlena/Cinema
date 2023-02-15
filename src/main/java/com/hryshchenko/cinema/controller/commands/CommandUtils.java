867e236592a555011842b30f9bf95a39410d3433
GrischenkoOlena <grischenko.olena.v@gmail.com>
2023-02-15 21:58:39 +0200

    refactoring code, some method moved to util's class

--- src/main/java/com/hryshchenko/cinema/controller/commands/CommandUtils.java
+++ src/main/java/com/hryshchenko/cinema/controller/commands/CommandUtils.java
@@ -2,7 +2,11 @@ package com.hryshchenko.cinema.controller.commands;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
+import static com.hryshchenko.cinema.constant.FieldName.*;
 
 public class CommandUtils {
 
@@ -25,4 +29,40 @@ public class CommandUtils {
         return locale;
     }
 
+    public static String getOrderBD(String order) {
+        return OrderMapUtil.getOrderBD(order);
+    }
+
+    static class OrderMapUtil {
+        private final static Map<String, String> orders = new HashMap<>();
+        static {
+            orders.put("movieAsc", VIEW_FILM_TITLE);
+            orders.put("movieDesc", VIEW_FILM_TITLE + " DESC");
+            orders.put("dataAsc", VIEW_FILM_DATE);
+            orders.put("dataDesc", VIEW_FILM_DATE + " DESC");
+            orders.put("availableAsc", VIEW_AMOUNT_FREE_SEATS);
+            orders.put("availableDesc", VIEW_AMOUNT_FREE_SEATS + " DESC");
+            orders.put("defaultScreening", VIEW_FILM_DATE + " DESC");
+
+            orders.put("nameAsc", USER_NAME);
+            orders.put("nameDesc", USER_NAME + " DESC");
+            orders.put("balanceAsc", USER_BALANCE);
+            orders.put("balanceDesc", USER_BALANCE + " DESC");
+            orders.put("defaultCustomer", USER_ID);
+
+            orders.put("titleAsc", FILM_TITLE);
+            orders.put("titleDesc", FILM_TITLE + " DESC");
+            orders.put("durationAsc", FILM_DURATION);
+            orders.put("durationDesc", FILM_DURATION + " DESC");
+            orders.put("defaultFilm", FILM_ID);
+
+            orders.put("dataTicketAsc", SCREENING_ID);
+            orders.put("dataTicketDesc", SCREENING_ID + " DESC");
+            orders.put("defaultTicket", TICKET_ID + " DESC");
+        }
+
+        public static String getOrderBD(String order) {
+            return "ORDER BY " + orders.get(order);
+        }
+    }
 }
