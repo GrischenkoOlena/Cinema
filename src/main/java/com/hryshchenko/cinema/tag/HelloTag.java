52ed934ab818c3e87bdae25e9d0d4f67486a2959
GrischenkoOlena <grischenko.olena.v@gmail.com>
2023-03-17 16:41:53 +0200

    i18n custom tag

--- src/main/java/com/hryshchenko/cinema/tag/HelloTag.java
+++ src/main/java/com/hryshchenko/cinema/tag/HelloTag.java
@@ -1,9 +1,13 @@
 package com.hryshchenko.cinema.tag;
 
+import com.hryshchenko.cinema.controller.commands.CommandUtils;
+
+import javax.servlet.http.HttpSession;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 public class HelloTag extends TagSupport {
     private String role;
@@ -15,10 +19,14 @@ public class HelloTag extends TagSupport {
     @Override
     public int doStartTag() throws JspException {
         String hello;
+
+        HttpSession session = pageContext.getSession();
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", CommandUtils.getLocale(session));
+
         try {
             JspWriter out = pageContext.getOut();
             if (role != null && !role.isEmpty()){
-                hello = "Welcome, " + role;
+                hello = bundle.getString("hello") + " " + role;
                 out.write(hello);
             }
         } catch (IOException e) {
